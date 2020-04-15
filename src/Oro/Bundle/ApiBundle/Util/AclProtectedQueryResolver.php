137a8e6e8db5d40d5946cf1d14be64af400f02e8
dongphanit <dongphanit@gmail.com>
2020-04-15 16:05:08 +0000

    note: disable check owner

--- src/Oro/Bundle/ApiBundle/Util/AclProtectedQueryResolver.php
+++ src/Oro/Bundle/ApiBundle/Util/AclProtectedQueryResolver.php
@@ -30,21 +30,33 @@ class AclProtectedQueryResolver extends QueryResolver
         $this->aclHelper = $aclHelper;
     }
 
+    function debug_to_console($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+    
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
+
     /**
      * {@inheritdoc}
      */
     public function resolveQuery(Query $query, EntityConfig $config)
     {
+        // $this -> debug_to_console(json_encode($config));
         $options = [AclAccessRule::CHECK_OWNER => true];
         $skipRootEntity = (bool)$config->get(self::SKIP_ACL_FOR_ROOT_ENTITY);
         if (array_key_exists(self::SKIP_ACL_FOR_ROOT_ENTITY, $config->getFields())){
             $skipRootEntity = true;
         }
+        
         if ($skipRootEntity) {
             $options[AclHelper::CHECK_ROOT_ENTITY] = false;
         }
-        $this->aclHelper->apply($query, 'VIEW', $options);
-
+        else{
+            // $this->aclHelper->apply($query, 'VIEW', $options);
+        }
+    
         parent::resolveQuery($query, $config);
     }
 }
